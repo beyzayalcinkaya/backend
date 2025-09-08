b1df26c2cd8ba521def1a67960f782fe71921774
beyzayalcinkaya <ylcnkybeyza666@gmail.com>
2025-09-09 00:03:59 +0300

    asdasdasd

--- index.js
+++ index.js
@@ -2,25 +2,18 @@ const express = require("express");
 const cors = require("cors");
 const { Pool } = require("pg");
 
-// PostgreSQL bağlantısı için Render ortam değişkenleri kullanılıyor
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: Number(process.env.DB_PORT), // Portu sayı olarak alıyoruz
-  ssl: {
-    rejectUnauthorized: false, // Render PostgreSQL için gerekli
-  },
+  user: "postgres",
+  host: "localhost",
+  database: "blog_data",
+  password: "1234",
+  port: 5439,
 });
-
-console.log(process.env, "asd");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Ana route
 app.get("/", (req, res) => {
   res.send("Backend çalışıyor!");
 });
@@ -38,12 +31,16 @@ app.get("/blogs", async (req, res) => {
 });
 
 // Tek blog
+
 app.get("/blogs/:id", async (req, res) => {
   const { id } = req.params;
+  console.log("Parametre gelen name:", id);
   try {
-    const result = await pool.query("SELECT * FROM myBlogs WHERE id = $1", [
-      id,
-    ]);
+    const result = await pool.query(
+      "SELECT * FROM myBlogs WHERE id = $1",
+
+      [id]
+    );
     console.log("Sorgu sonucu:", result.rows);
 
     if (result.rows.length === 0) {
@@ -53,11 +50,10 @@ app.get("/blogs/:id", async (req, res) => {
 
     res.json(result.rows[0]);
   } catch (err) {
-    console.error("Tek blog hatasıı:", err);
+    console.error("Tek blog hatası:", err);
     res.status(500).send("Database error");
   }
 });
 
-// Render port veya default 3002
-const PORT = process.env.PORT || 3002;
+const PORT = 3002;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
